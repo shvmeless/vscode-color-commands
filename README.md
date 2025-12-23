ac17b00ab0e83c4d902c7b7f3258f0c371b603c4
Luciano Aedo Puga <81434301+shvmeless@users.noreply.github.com>
2025-12-23 12:53:12 -0300

    chore: initialize the project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Color Commands
+
+This is an extension for Visual Studio Code that adds a series of commands for performing color operations in the editor.
---
+++ src/extension.ts
@@ -0,0 +1,12 @@
+// IMPORTS
+import * as vscode from 'vscode';
+
+// ACTIVATE
+export function activate() {
+
+	console.log('The extension "vscode-color-comands" is now active!');
+
+}
+
+// DEACTIVATE
+export function deactivate() {}
